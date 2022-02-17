a137aab75b2ff856c025d2504bed30f99cdcbfca
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-02-16 23:44:31 -0600

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -32,8 +32,6 @@ echo -e "${verde}┌════════════════════
 └═══════════════════════════════════┘${blanco}"
 echo -e ""
 sleep 0.5
-PWD=$(pwd)
-source ${PWD}/data/data/com.termux/files/home
 apt update && pkg upgrade -y
 apt install git curl wget nmap ruby -y
 sleep 0.5
