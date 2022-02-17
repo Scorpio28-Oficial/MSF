e0b9c97c09918b953855fcf86db486e3dda50007
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-02-16 19:47:35 -0600

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -2,7 +2,7 @@
 #
 # Created by: Scorpio28
 #
-# Ruby_v2.7.2
+# MSF
 #
 # VARIABLES
 #
@@ -11,12 +11,12 @@ source ${PWD}/Colors.sh
 #
 # FUNCIONES
 #
-function MSF_v2.7.2 {
+function MSF {
 sleep 0.1
 }
 #
 # CÓDIGO
-sleep 0.5
+sleep 1
 echo -e "${verde}
 ███╗   ███╗███████╗███████╗
 ████╗ ████║██╔════╝██╔════╝
@@ -25,7 +25,7 @@ echo -e "${verde}
 ██║ ╚═╝ ██║███████║██║     
 ╚═╝     ╚═╝╚══════╝╚═╝     
 "${blanco}
-sleep 1
+sleep 0.5
 echo -e "${verde}┌═════════════════════════════════┐
 █${blanco} INSTALADO PAQUETES REQUERIDOS...${verde}█
 └═════════════════════════════════┘${blanco}"
@@ -34,58 +34,9 @@ sleep 0.5
 PWD=$(pwd)
 source ${PWD}/data/data/com.termux/files/home
 apt update && pkg upgrade -y
-apt install -y python
-pkg install -y nmap
-pkg install -y ruby
-pkg install -y php
-pkg install -y tor
-pkg install -y openssh
-pkg install -y openssl
-pkg install -y curl
-pkg install -y perl
-pip install --upgrade pip
-easy_install shodan
-pip install shodan requests
-pkg install -y wget
-pkg install -y clang
-pkg install -y pv
-pkg install -y fish
-pkg install -y bash
-pkg install -y nano
-pkg install -y w3m
-pkg install -y figlet
-pkg install -y cowsay
-pkg install -y wcalc
-pkg install -y toilet
-pkg install -y net-tools
-pkg install -y cmatrix
-pkg install -y vim
-pkg insnall -y jq
-pkg install -y nodejs-lts
-pkg install -y grep
-yes | pkg install netcat-openbsd
-yes | pkg install mutt
-yes | pkg install nodejs
-apt-get install -y espeak
-apt install git curl wget nmap -y
-apt-get install -y espeak
+apt install git curl wget nmap ruby -y
 sleep 0.5
-echo -e "${verde}
-┌═══════════════════════════┐
-█ ${blanco}INSTALANDO RUBY v2.7.2... ${verde}█
-└═══════════════════════════┘
-"${blanco}
 echo -e ""
-sleep 0.5
-pkg unistall -y ruby &> /dev//null
-bash <(curl -fsSL "https://git.io/abhacker-repo") --install ruby=2.7.22 &> /dev//null
-sleep 0.5
-echo -e "${verde}
-┌═══════════════════════┐
-█ ${blanco}RUBY v2.7.2 INSTALADO ${verde}█
-└═══════════════════════┘
-"${blanco}
-sleep 0.5
 echo -e "${verde}
 MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 MMMMMMMMMMM                MMMMMMMMMM
@@ -108,7 +59,7 @@ MMMMNNMNMMMMMNx        MMMMMMNMMNMMNM
 MMMMMMMMNMMNMMMMm+..+MMNMMNMNMMNMMNMM
 "${blanco}
 echo -e ""
-sleep 0.5
+sleep 1
 echo -e "${verde}
 ┌═════════════════════════════════┐
 █${blanco} INSTALANDO METASPLOIT-FRAMEWORK ${verde}█
