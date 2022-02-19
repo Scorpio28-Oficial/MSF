af0b80e77bd57719df0b1b39a973f790d0a3cb3e
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-02-18 20:47:05 -0600

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -81,7 +81,7 @@ echo -e "${verde}MMMMMMMMMMNm,            eMMMMMNMMNMM"
 sleep 0.1
 echo -e "${verde}MMMMNNMNMMMMMNx        MMMMMMNMMNMMNM"
 sleep 0.1
-echo -e "${verde}MMMMMMMMNMMNMMMMm+..+MMNMMNMNMMNMMNMM"
+echo -e "${verde}MMMMMMMMNMMNMMMMm+..+MMNMMNMNMMNMMNMM
 "${blanco}
 sleep 0.5
 echo -e ""
