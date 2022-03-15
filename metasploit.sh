e83b0254bbb677f6739d1401487987c52537331e
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-03-14 21:28:36 -0600

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -34,17 +34,6 @@ echo -e "${verde}
 echo -e ""
 sleep 0.5
 pkg install curl wget php openssh openssl -y
-sleep 0.5
-echo -e ""
-sleep 1
-echo -e "${verde}
-┌═════════════════════════════════┐
-█${blanco} INSTALANDO METASPLOIT-FRAMEWORK ${verde}█
-└═════════════════════════════════┘
-"${blanco}
-sleep 0.5
-echo -e ""
-#!/data/data/com.termux/files/usr/bin/bash
 sleep 1
 echo -e "${verde}MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
 sleep 0.1
@@ -85,7 +74,32 @@ sleep 0.1
 echo -e "${verde}MMMMMMMMNMMNMMMMm+..+MMNMMNMNMMNMMNMM"${blanco}
 sleep 0.5
 echo -e ""
+sleep 1
+echo -e "${verde}
+┌═════════════════════════════════┐
+█${blanco} INSTALANDO METASPLOIT-FRAMEWORK ${verde}█
+└═════════════════════════════════┘
+"${blanco}
+sleep 0.5
+echo -e "${verde}
+███╗   ███╗███████╗███████╗
+████╗ ████║██╔════╝██╔════╝
+██╔████╔██║███████╗█████╗
+██║╚██╔╝██║╚════██║██╔══╝
+██║ ╚═╝ ██║███████║██║
+╚═╝     ╚═╝╚══════╝╚═╝"${blanco}
+sleep 0.5
 echo -e ""
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+echo "
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+    |M|e|t|a|s|p|l|o|i|t| |i|n| |T|e|r|m|u|x|
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+                +-+-+ +-+-+-+-+-+-+-+
+     BUG FIXED  |b|y| |D|E|D|S|H|I|T|
+                +-+-+ +-+-+-+-+-+-+-+
+"
 
 center() {
   termwidth=$(stty size | cut -d" " -f2)
