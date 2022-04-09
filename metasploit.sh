1215078ef4dd2e2da04a797332bb35fb92e38a1d
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-04-09 10:08:56 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -217,8 +217,6 @@ sed -i '13,15 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hr
 sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp256.rb
 sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp384.rb
 sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp521.rb
-cd $HOME/metasploit-framework
-bundle install
 sleep 0.5
 echo -e ""
 echo -e "${verde}
