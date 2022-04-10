8dc015ccd6628e43a70dd30b04e7d8b3977b6f56
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-04-10 14:50:34 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -89,50 +89,38 @@ echo -e "${verde}MMMMNNMNMMMMMNx        MMMMMMNMMNMMNM"
 sleep 0.1
 echo -e "${verde}MMMMMMMMNMMNMMMMm+..+MMNMMNMNMMNMMNMM"${blanco}
 echo -e ""
-echo -e ""
-echo -e ""
 sleep 0.5
+echo -e ""
+echo -e "[${verde}+${clear}] Have you cleaned your termux? if no script may not work..."
 
-center() {
-  termwidth=$(stty size | cut -d" " -f2)
-  padding="$(printf '%0.1s' ={1..500})"
-  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
-}
+echo -e ""
+echo -e "[${amarillo}+${clear}] Please wait..."
+sleep 3
+echo "[+] Updating termux..."
+sleep 3
+echo -e "[${amarillo}+${clear}] Don't hit Enter use Y/y to update..."
+sleep 3
+
+echo -e ""
+sleep 3
+pkg update;pkg upgrade
+echo -e ""
+echo -e "[${amarillo}+${clear}] Termux updated..."
+echo -e "[${amarillo}+${clear}] Please wait..."
+sleep 3
+
 #
 # Loading spinner
-center " Loading..."
+echo -e "[${amarillo}+${clear}] Loading..."
 source <(echo "c3Bpbm5lcj0oICd8JyAnLycgJy0nICdcJyApOwoKY291bnQoKXsKICBzcGluICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9uZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBpIGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcciRpIjsKICAgICAgc2xlZXAgMC4yOwogICAgZG9uZTsKICBkb25lCn0KCmNvdW50" | base64 -d)
 
-echo
-center "*** Dependencies installation..."
+echo -e ""
+echo -e "[${amarillo}+${clear}]Dependencies wget, curl, zip tar, etc..."
 
-# Remove not working repositories
-rm $PREFIX/etc/apt/sources.list.d/*
-
-# Install gnupg required to sign repository
-pkg install -y gnupg
-
-# Sign gushmazuko repository
-curl -fsSL https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/gushmazuko-gpg.pubkey | gpg --dearmor | tee $PREFIX/etc/apt/trusted.gpg.d/gushmazuko-repo.gpg
-
-# Add gushmazuko repository to install ruby 2.7.2 version
-echo 'deb https://github.com/gushmazuko/metasploit_in_termux/raw/master gushmazuko main'  | tee $PREFIX/etc/apt/sources.list.d/gushmazuko.list
-
-# Set low priority for all gushmazuko repository (for security purposes)
-# Set highest priority for ruby package from gushmazuko repository
-echo '## Set low priority for all gushmazuko repository (for security purposes)
-Package: *
-Pin: release gushmazuko
-Pin-Priority: 100
-
-## Set highest priority for ruby package from gushmazuko repository
-Package: ruby
-Pin: release gushmazuko
-Pin-Priority: 1001' | tee $PREFIX/etc/apt/preferences.d/preferences
 
 # Purge installed ruby
 apt purge ruby -y
-rm -fr $PREFIX/lib/ruby/gems
+rm -rf $PREFIX/lib/ruby
 
 pkg upgrade -y -o Dpkg::Options::="--force-confnew"
 pkg install -y python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
@@ -140,21 +128,18 @@ pkg install -y python autoconf bison clang coreutils curl findutils apr apr-util
 python3 -m pip install --upgrade pip
 python3 -m pip install requests
 
-echo
-center "*** Fix ruby BigDecimal"
+echo -e ""
+echo -e "[${amarillo}+${clear}]Fix ruby BigDecimal..."
 source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
 
-echo
-center "*** Erasing old metasploit folder..."
-rm -rf $HOME/metasploit-framework
-
-echo
-center "*** Downloading..."
+echo -e ""
+echo -e ""
+echo -e "[${amarillo}+${clear}] Downloading the script..."
 cd $HOME
 git clone https://github.com/rapid7/metasploit-framework.git --depth=1
 
-echo
-center "*** Installation..."
+echo -e ""
+echo -e "[${amarillo}+${clear}] Installation..."
 cd $HOME/metasploit-framework
 sed '/rbnacl/d' -i Gemfile.lock
 sed '/rbnacl/d' -i metasploit-framework.gemspec
@@ -162,13 +147,14 @@ sed '/rbnacl/d' -i metasploit-framework.gemspec
 # version 0.118
 # root cause for this problem is missing net-smtp & mini_portile2 version
 
+# edit: they added net-smtp in gemspec
+
 # Warnings were fixed 
 
-echo 
-center "《《《  MSF FIX 》》》"
-
-export MSF_FIX="spec.add_runtime_dependency 'net-smtp'"
-sed -i "146i \  $MSF_FIX" metasploit-framework.gemspec
+# looks like someone added this in gemspec 
+# hereafter no need to add thus dependency :D
+#export MSF_FIX="spec.add_runtime_dependency 'net-smtp'"
+#sed -i "146i \  $MSF_FIX" metasploit-framework.gemspec
 sed -i "277,\$ s/2.8.0/2.2.0/" Gemfile.lock
 
 gem install bundler
@@ -191,9 +177,8 @@ fi
 ln -s $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
 termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
 
-echo
-center "*"
-echo -e "\033[32m Suppressing Warnings\033[0m"
+echo -e ""
+echo -e "[${amarillo}+${clear}] Suppressing Warning"
 
 sed -i '355 s/::Exception, //' msfvenom
 sed -i '481, 483 {s/^/#/}' msfvenom
@@ -202,10 +187,31 @@ sed -i '86 {s/^/#/};96 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3
 sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/logging-2.3.0/lib/logging/diagnostic_context.rb
 ln -s $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
 
-echo
-center "*"
-echo -e "\033[32m Installation complete. \n Launch metasploit by executing: msfconsole\033[0m"
-center "*"
+echo -e ""
+clear
+echo -e ""
+
+sleep 3
+echo -e ""
+echo -e "[${amarillo}+${clear}] ${blanco} Please open new terminal and type ${cyan}msfconsole${clear}. Is it Working?${clear} [${amarillo}y/n${clear}]:"
+read replyme
+if [[ $replyme == 'y' ]]
+then
+echo -e "[${amarillo}+${clear}] Cheers! Metasploit V6 1.38 has been installed..."
+sleep 2
+echo -e "[${amarillo}+${clear}] See you again..."
+else
+echo -e "[${rojo}+${clear}]${rojo}I'm sorry you pressed no...${clear} "
+sleep 1
+echo -e "[${amarillo}+${clear}] Fixing...."
+  sleep 3
+echo -e "[${amarillo}+${clear}] Fixing...."
+  sleep 3
+sed -i '13,14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb;sed -i '15 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb;sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp256.rb;sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp384.rb;sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp521.rb
+
+echo -e ""
+echo -e "[${amarillo}+${clear}] ------------Ttry msfconsole..."
+fi
 
 cd $HOME/metasploit-framework
 sed -i '13,15 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb
