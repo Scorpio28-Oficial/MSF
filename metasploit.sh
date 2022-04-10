3ca1b63d5c80f738dac96ea077b4f348bc5974d6
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-04-10 15:55:01 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -213,13 +213,6 @@ echo -e ""
 echo -e "${verde}[${rojo}+${verde}] ------------Ttry msfconsole...${blanco}"
 fi
 
-cd $HOME/metasploit-framework
-sed -i '13,15 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb
-sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp256.rb
-sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp384.rb
-sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp521.rb
-sleep 0.5
-echo -e ""
 echo -e "${verde}
 ┌════════════════════════════════┐
 █${blanco} METASPLOIT-FRAMEWORK INSTALADO${verde} █
