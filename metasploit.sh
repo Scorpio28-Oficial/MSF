00a28a974cdeee79ad90b3f2cfaf13bc3a56cfc5
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-04-11 19:41:18 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -184,11 +184,11 @@ echo -e ""
 
 sleep 3
 echo -e ""
-echo -e "${verde}[${rojo}+${verde}] Please open new terminal and type ${blanco}msfconsole.${verde} Is it Working? [${blanco}y${verde}/${blanco}n${verde}]:${blanco}"
+echo -e "${verde}[${rojo}+${verde}] Please press the letter 'y' of enter to finish the installation [n/y].${blanco}"
 read replyme
 if [[ $replyme == 'y' ]]
 then
-echo -e "${verde}[${rojo}+${verde}] Cheers! Metasploit V6 1.38 has been installed...${blanco}"
+echo -e "${verde}[${rojo}+${verde}] Cheers! Metasploit V6.1.38 has been installed...${blanco}"
 sleep 2
 echo -e "${verde}[${rojo}+${verde}] See you again...${blanco}"
 else
