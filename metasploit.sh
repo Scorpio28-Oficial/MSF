e6d4ab3ccbe4e322c77938360939fd709e58fd95
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-05-15 20:48:07 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -202,7 +202,7 @@ if [[ $replyme == 'y' ]];then
 echo -e ""
 echo -e "${rojo}[${verde}√]${rojo}]${verde} Metasploit V6.1.43 Has Been Installed...${blanco}"
 sleep 2
-echo -e "${rojo}[${verde}*${rojo}]${verde} See You Again...${blanco}"
+echo -e "${rojo}[${verde}√${rojo}]${verde} See You Again...${blanco}"
 else
 echo -e "${rojo}[${verde}*${rojo}]${verde} I'm Sorry You Pressed No...${blanco} "
 sleep 1
