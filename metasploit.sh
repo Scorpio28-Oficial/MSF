89267fb0966c05316547b1a8b4f44c9389092757
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-06-08 09:48:12 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -20,17 +20,12 @@ sleep 1
 echo -e ""
 sleep 0.1
 echo -e "
-echo -e "${rojo}        __  __ ____  ${verde}_____"
-sleep 0.1
-echo -e "${rojo}        |  \/  / ___|${verde}|  ___|"
-sleep 0.1
-echo -e "${rojo}        | |\/| \___ \${verde}| |_"
-sleep 0.1
-echo -e "${rojo}        | |  | |___) ${verde}|  _|"
-sleep 0.1
-echo -e "${rojo}        |_|  |_|____/|_${verde}|"
-sleep 0 1
-echo -e "${bicolor}.:.:. Metasploit-framework .:.:."${blanco}
+${rojo}        __  __ ____  ${verde}_____
+${rojo}        |  \/  / ___|${verde}|  ___|
+${rojo}        | |\/| \___ \${verde}| |_
+${rojo}        | |  | |___) ${verde}|  _|
+${rojo}        |_|  |_|____/|_${verde}|
+${bicolor}.:.:. Metasploit-framework .:.:."${blanco}
 sleep 0.5
 echo -e ""
 echo -e ""
