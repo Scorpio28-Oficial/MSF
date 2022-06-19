19dab784accfce8c0fe0d4627c38139b21dff7f3
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-06-18 22:57:04 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -200,7 +200,7 @@ echo -e -n "${rojo}[${verde}*${rojo}]${verde} Please Press The Letter 'y' Of Ent
 read replyme
 if [[ $replyme == 'y' ]];then
 echo -e ""
-echo -e "${rojo}[${verde}√]${rojo}]${verde} Cheers! Metasploit V6.2.3 Has Been Installed...${blanco}"
+echo -e "${rojo}[${verde}√]${rojo}]${verde} Cheers! Metasploit V6.2.4 Has Been Installed...${blanco}"
 sleep 2
 echo -e "${rojo}[${verde}*${rojo}]${verde} See You Again...${blanco}"
 else
