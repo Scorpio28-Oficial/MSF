b461954997f37d1b2da5a7cdadec4dc73234a6f2
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-08-19 00:09:03 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -225,10 +225,6 @@ else
 echo -e "${rojo}[${verde}*${rojo}]${verde} I'M SORRY YOU PRESSED NO...${blanco} "
 sleep 1
 echo -e "${rojo}[${verde}√${rojo}]${verde} FINALIZED....${blanco}"
-sleep 1
-echo -e "${rojo}[${verde}√${rojo}]${verde} FINALIZED....${blanco}"
-sleep 1
-echo -e "${rojo}[${verde}√${rojo}]${verde} FINALIZED....${blanco}"
 sleep 3
 sed -i '13,14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb;sed -i '15 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb;sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp256.rb;sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp384.rb;sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp521.rb
 
