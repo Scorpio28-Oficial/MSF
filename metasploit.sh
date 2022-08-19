7695f63a024b363f6dad65e416d0119e518d2b78
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-08-19 07:55:53 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -209,9 +209,7 @@ echo -e "${verde}██║ ╚═╝ ██║███████║██║"
 sleep 0.1
 echo -e "${verde}╚═╝     ╚═╝╚══════╝╚═╝"${blanco}
 echo -e ""
-echo -e "${rojo}[${verde}*${rojo}]${verde} JOIN OUR COMMUNITY IN TELEGRAM${blanco}"
- 
-echo -e "${blanco}Informatic in Termux / WHITE HACKS"
+echo -e "${rojo}[${verde}*${rojo}]${blanco} Informatic in Termux / WHITE HACKS"
 sleep 3
 echo -e ""
 echo -e -n "${rojo}[${verde}√${rojo}]${verde} PLEASE PRESS THE LETTER '${blanco}y${verde}' OF ENTER TO FINISH THE INTALLATION [${blanco}n${verde}/${blanco}y${verde}]: ${blanco}"
@@ -229,9 +227,7 @@ sleep 3
 sed -i '13,14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb;sed -i '15 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb;sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp256.rb;sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp384.rb;sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp521.rb
 
 echo -e ""
-echo -e "${rojo}[${verde}*${rojo}]${verde} JOIN OUR COMMUNITY IN TELEGRAM${blanco}"
- 
-echo -e "${blanco}Informatic in Termux / WHITE HACKS"
+echo -e "${rojo}[${verde}*${rojo}]${blanco} Informatic in Termux / WHITE HACKS"
 fi
 
 cd $HOME/metasploit-framework
