dad8a0a3b40a8809679b9a350f029a76541e5883
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-08-19 22:15:15 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -211,7 +211,8 @@ echo -e "${verde}╚═╝     ╚═╝╚══════╝╚═╝"${blan
 sleep 0.1
 echo -e ""
 sleep 0.1
-echo -e "${verde}INFORMATIC IN TERMUX / WHITE HACKS
+echo -e "${verde}-------C-O-M-M-U-N-I-T-I-E-S-------
+INFORMATIC IN TERMUX / WHITE HACKS
 ----------------[+]----------------"
 sleep 3
 echo -e ""
