e0321d9c181efb808f502483b9e2f1f58549767f
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-09-28 20:43:47 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -246,6 +246,10 @@ sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_r
 sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp384.rb
 sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp521.rb
 sleep 0.5
+else
+echo -e "$replyme"
+sleep 0.5
 echo -e ""
 echo -e "${rojo}[${amarillo}√${rojo}]${verde} MSF Installed.${blanco}"
 echo -e "${rojo}[${amarillo}√${rojo}]${verde} Open The Console With: ${blanco}msfconsole${blanco}"
+fi
