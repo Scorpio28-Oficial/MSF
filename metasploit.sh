622e67e2ef90d71d2c963a44bde44fe4dc58d1d7
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-10-08 16:46:48 -0500

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -37,10 +37,11 @@ echo -e ""
 sleep 1
 echo -e "${rojo}[${amarillo}*${rojo}]${verde} Installing Required Packages...${blanco}"
 sleep 0.5
-apt update && apt upgrade -y
-pkg install wget curl openssh openssl git -y
+yes|apt update && apt upgrade
+yes|pkg uninstall ruby
+yes|pkg install wget curl openssh openssl git
 apt install ncurses-utils
-pkg install wget
+yes|pkg install wget
 echo -e ""
 echo -e ""
 sleep 1
@@ -110,6 +111,12 @@ echo -e "${rojo}[${amarillo}*${rojo}]${verde} Please Be Patient...${blanco}"
 sleep 3
 echo -e ""
 
+center() {
+  termwidth=$(stty size | cut -d" " -f2)
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
 #
 # Loading spinner
 echo -e -n "${rojo}[${amarillo}*${rojo}]${verde} Loading.....${amarillo}"
@@ -118,85 +125,119 @@ ICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9u
 ZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBp
 IGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcclx0XHQkaSI7CiAg
 ICAgIHNsZWVwIDAuMjsKICAgIGRvbmU7CiAgZG9uZQp9Cgpjb3VudAo=" | base64 -d)
-
-echo -e ""
-sleep 1
 echo -e ""
 echo -e "${rojo}[${amarillo}*${rojo}]${verde} Dependencies wget, curl, zip tar, etc...${blanco}"
 
+## Remove not working repositories
+rm $PREFIX/etc/apt/sources.list.d/*
+
+## Install gnupg required to sign repository
+# pkg install -y gnupg
+
+## Sign gushmazuko repository
+# curl -fsSL https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/gushmazuko-gpg.pubkey | gpg --dearmor | tee $PREFIX/etc/apt/trusted.gpg.d/gushmazuko-repo.gpg
+
+## Add gushmazuko repository to install ruby 2.7.2 version
+# echo 'deb https://github.com/gushmazuko/metasploit_in_termux/raw/master gushmazuko main'  | tee $PREFIX/etc/apt/sources.list.d/gushmazuko.list
+
+## Set low priority for all gushmazuko repository (for security purposes)
+## Set highest priority for ruby package from gushmazuko repository
+# echo '## Set low priority for all gushmazuko repository (for security purposes)
+# Package: *
+# Pin: release gushmazuko
+# Pin-Priority: 100
+
+## Set highest priority for ruby package from gushmazuko repository
+# Package: ruby
+# Pin: release gushmazuko
+# Pin-Priority: 1001' | tee $PREFIX/etc/apt/preferences.d/preferences
 
 # Purge installed ruby
 apt purge ruby -y
-rm -rf $PREFIX/lib/ruby
+rm -fr $PREFIX/lib/ruby/gems
 
 pkg upgrade -y -o Dpkg::Options::="--force-confnew"
-pkg install -y python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
+
+# needs binutils
+pkg install -y binutils python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
 
 python3 -m pip install --upgrade pip
 python3 -m pip install requests
 
-echo -e ""
-sleep 1
+
+# if any weird warning occurs maybe its becoze of bigdecimal & pg_ext.so . try comment those lines if problem persist
+
 echo -e ""
 echo -e "${rojo}[${amarillo}*${rojo}]${verde} Fix Ruby BigDecimal...${blanco}"
 source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
 
 echo -e ""
-slepp 1
+rm -rf $PREFIX/opt/metasploit-framework
+
 echo -e ""
 echo -e "${rojo}[${amarillo}*${rojo}]${verde} Downloading The Script...${blanco}"
-cd $HOME
+cd $PREFIX/opt
 git clone https://github.com/rapid7/metasploit-framework.git --depth=1
 
 echo -e ""
-sleep 1
 echo -e "${rojo}[${amarillo}*${rojo}]${verde} Installing Gems...${blanco}"
-cd $HOME/metasploit-framework
-sed '/rbnacl/d' -i Gemfile.lock
-sed '/rbnacl/d' -i metasploit-framework.gemspec
+cd $PREFIX/opt/metasploit-framework
+# sed '/rbnacl/d' -i Gemfile.lock
+# sed '/rbnacl/d' -i metasploit-framework.gemspec
 
-# version 0.118
-# root cause for this problem is missing net-smtp & mini_portile2 version
-
-# edit: they added net-smtp in gemspec
-
-# Warnings were fixed 
-
-# looks like someone added this in gemspec 
-# hereafter no need to add thus dependency :D
-#export MSF_FIX="spec.add_runtime_dependency 'net-smtp'"
-#sed -i "146i \  $MSF_FIX" metasploit-framework.gemspec
-sed -i "277,\$ s/2.8.0/2.2.0/" Gemfile.lock
+#sed -i "277,\$ s/2.8.0/2.2.0/" Gemfile.lock
 
 gem install bundler
-sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+declare NOKOGIRI_VERSION=$(cat Gemfile.lock | grep -i nokogiri | sed 's/nokogiri [\(\)]/(/g' | cut -d ' ' -f 5 | grep -oP "(.).[[:digit:]][\w+]?[.].")
+#sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
 
-gem install nokogiri -v 1.8.0 -- --use-system-libraries
+gem install nokogiri -v $NOKOGIRI_VERSION -- --use-system-libraries
+
+# for aarch64 if nokogiri problem persist do this 
+
+bundle config build.nokogiri "--use-system-libraries --with-xml2-include=$PREFIX/include/libxml2"; bundle install
 
 gem install actionpack
 bundle update activesupport
 bundle update --bundler
 bundle install -j$(nproc --all)
-$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
-rm ./modules/auxiliary/gather/http_pdf_authors.rb
+
+#$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+# rm ./modules/auxiliary/gather/http_pdf_authors.rb
 if [ -e $PREFIX/bin/msfconsole ];then
 	rm $PREFIX/bin/msfconsole
 fi
 if [ -e $PREFIX/bin/msfvenom ];then
 	rm $PREFIX/bin/msfvenom
 fi
-ln -s $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
-termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
+if [ -e $PREFIX/bin/msfrpcd ];then
+	rm $PREFIX/bin/msfrpcd
+fi
+ln -s $PREFIX/opt/metasploit-framework/msfconsole $PREFIX/bin/
+ln -s $PREFIX/opt/metasploit-framework/msfvenom $PREFIX/bin/
+ln -s $PREFIX/opt/metasploit-framework/msfrpcd $PREFIX/bin/
+
+termux-elf-cleaner $PREFIX/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
 
 echo -e ""
 echo -e "${rojo}[${amarillo}*${rojo}]${verde} Supprssing Warning${blanco}"
 
-sed -i '355 s/::Exception, //' msfvenom
-sed -i '481, 483 {s/^/#/}' msfvenom
-sed -Ei "s/(\^\\\c\s+)/(\^\\\C-\\\s)/" /data/data/com.termux/files/home/metasploit-framework/lib/msf/core/exploit/remote/vim_soap.rb
-sed -i '86 {s/^/#/};96 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
-sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/logging-2.3.0/lib/logging/diagnostic_context.rb
-ln -s $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+# sed -i '355 s/::Exception, //' $PREFIX/bin/msfvenom
+# sed -i '481, 483 {s/^/#/}' $PREFIX/bin/msfvenom
+
+
+# sed -Ei "s/(\^\\\c\s+)/(\^\\\C-\\\s)/" $PREFIX/opt/metasploit-framework/lib/msf/core/exploit/remote/vim_soap.rb
+
+# Warning occurs during payload generation
+sed -i '86 {s/^/#/};96 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
+sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/logging-2.3.1/lib/logging/diagnostic_context.rb
+
+## openssl issue has been fixed 
+
+#sed -i '13,15 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb
+#sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp256.rb
+#sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp384.rb
+#sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp521.rb
 
 echo -e ""
 clear
