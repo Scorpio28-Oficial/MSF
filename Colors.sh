f967790eb6e266dad5337b6655fbfe45193e8c66
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2023-08-19 09:11:27 -0600

    Update Colors.sh

--- Colors.sh
+++ Colors.sh
@@ -3,6 +3,7 @@
 export rosa='\033[38;5;207m'
 export rojo='\033[1;31m'
 export verde='\033[1;32m'
+export verde_oscuro='\033[0;32m'
 export amarillo='\033[33m'
 export azul='\033[34m'
 export morado='\033[35m'
