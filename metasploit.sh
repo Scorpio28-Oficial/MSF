18ae353e178391f224c2e834ff69593066d470b5
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2023-08-19 09:23:02 -0600

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -267,4 +267,4 @@ echo -e ""
 sleep 3
 echo -e "${verde}[${blanco}√${verde}]${gris_oscuro} MSF Installed v6.3.31-dev-8b8acad.${blanco}"
 sleep 1
-echo -e "${verde}[${blanco}√${verde}]${gris_oscuro} Open The Console With: ${blanco}msfconsole${blanco}"
+echo -e "${verde}[${blanco}√${verde}]${gris_oscuro} Open The Console With: ${bicolor}msfconsole${blanco}"
