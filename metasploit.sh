944aedaeffafd4a46f6529e9b3e5b2381a7ab4da
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2023-10-16 16:37:29 -0600

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -280,10 +280,10 @@ sleep 1
 while :
 do
 echo -e -n "${verde}[${blanco}*${verde}]${blanco} SÉLECTIONNEZ UNE DES TROIS OPTIONS:
-${verde}[${blanco}*${verde}]${blanco} OPEN CONSOLE ━━━━━━━━━━━═>>> y
-${verde}[${blanco}*${verde}]${blanco} GO OUT ━━━━━━━━━━━━━━━━═>>> n
-${verde}[${blanco}*${verde}]${blanco} REINSTALL ━━━━━━━━━━━━━━═>>> r
-${verde}[${blanco}*${verde}]${blanco} CHOOSE ONE OPTION ━━━━━━━═>>>${cierre} "
+${verde}[${blanco}+${verde}]${blanco} OPEN CONSOLE >>>>>>>>>>>>>>>>>>>>> y
+${verde}[${blanco}+${verde}]${blanco} GO OUT >>>>>>>>>>>>>>>>>>>>>>>>>>> n
+${verde}[${blanco}+${verde}]${blanco} REINSTALL >>>>>>>>>>>>>>>>>>>>>>>> r
+${verde}[${blanco}+${verde}]${blanco} CHOOSE ONE OPTION ━━━━━━━═>>>${cierre} "
 
 read -r Opcion_Reiniciar
 
