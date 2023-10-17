038318868f2cc9e0f590589754b52f18ff8e31f6
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2023-10-16 21:29:56 -0600

    Update and rename msfconsole to msfconsole.sh

---
+++ msfconsole.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Created by: Scorpio28
+#
+# msfconsole
+#
+# VARIABLES
+#
+PWD=$(pwd)
+source ${PWD}/Colors.sh
+#
+# FUNCIONES
+#
+function msfconsole {
+sleep 0.1
+}
+#
+# CÓDIGO
+msfconsole
